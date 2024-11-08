52621c4a4ebd5e8bdeb4a37c7da86d3b778d3458
Ayush karwa <ayushkarwa18@gmail.com>
2024-11-09 01:32:39 +0530

    added jwt auth

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
-    
-    const {Router} = require("express");
+const dotenv = require("dotenv")
+dotenv.config()
+const jwt = require("jsonwebtoken")    
+const {Router} = require("express");
 const { UserModel } = require("../db");
-    const userRouter = Router()
-    const JWT_SECRET = "ilovekiara"
+const {auth }= require("../auth/auth");
+const userRouter = Router()
+    
     
 
     userRouter.post("/signup",async(req,res)=>{
@@ -20,19 +23,19 @@ const { UserModel } = require("../db");
         })
     })
     
-    userRouter.post("/signin",(req,res)=>{
+    userRouter.post("/signin",async(req,res)=>{
 
         const {email,password} = req.body
-        const user = UserModel.findOne({
+        const user = await UserModel.findOne({
             email:email,
             password:password
         })
-        console.log("user is: "+ user)
+        console.log("user is: "+ user+ " user id is: "+user._id.toString())
         if(user){ 
 
         const token = jwt.sign({
-            id: user._id.toString()
-        },JWT_SECRET)
+            id: user._id
+        },process.env.JWT_SECRET)
         console.log("token is: "+token)
         res.json({
             message: "you are signed in successfully!",
@@ -41,7 +44,7 @@ const { UserModel } = require("../db");
     }
     })
 
-    userRouter.post("/purchases",(req,res)=>{
+    userRouter.post("/purchases",auth,(req,res)=>{
         res.json({
             message: "you are signed up!"
         })
