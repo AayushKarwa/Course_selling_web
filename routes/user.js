2ad2ce7d84c49431add1a5da02f32bd78bbe31c7
Ayush karwa <ayushkarwa18@gmail.com>
2024-11-09 00:45:38 +0530

    adding jwt

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,44 @@
-    const {Router} = require("express")
+    
+    const {Router} = require("express");
+const { UserModel } = require("../db");
     const userRouter = Router()
+    const JWT_SECRET = "ilovekiara"
+    
 
-    userRouter.post("/signup",(req,res)=>{
+    userRouter.post("/signup",async(req,res)=>{
+
+        const {email,password,firstname,lastname } = req.body;
+
+        await UserModel.create({
+            email:email,
+            password:password,
+            firstname:firstname,
+            lastname:lastname
+        })
         res.json({
             message: "you are signed up!"
         })
     })
     
     userRouter.post("/signin",(req,res)=>{
-        res.json({
-            message: "you are signed up!"
+
+        const {email,password} = req.body
+        const user = UserModel.findOne({
+            email:email,
+            password:password
         })
+        console.log("user is: "+ user)
+        if(user){ 
+
+        const token = jwt.sign({
+            id: user._id.toString()
+        },JWT_SECRET)
+        console.log("token is: "+token)
+        res.json({
+            message: "you are signed in successfully!",
+            token: token
+        })
+    }
     })
 
     userRouter.post("/purchases",(req,res)=>{
