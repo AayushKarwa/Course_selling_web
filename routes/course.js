3e468100f01c7c6f18d21b305b9a9d97ce4c9b97
Ayush karwa <ayushkarwa18@gmail.com>
2024-11-09 05:08:20 +0530

    added purchases enpoints

--- routes/course.js
+++ routes/course.js
@@ -1,15 +1,43 @@
-    const {Router} = require("express")
+require("dotenv").config()
+const {Router} = require("express")
+const { middleware } = require("../auth/middleware")
+const { PurchaseModel, CourseModel } = require("../db")
     const courseRouter = Router()
 
-    courseRouter.post("/purchase",(req,res)=>{
+    courseRouter.post("/purchase",middleware(process.env.JWT_USER_SECRET),async(req,res)=>{
+        const userId = req.userid
+        const {courseid} = req.body
+
+        await PurchaseModel.create({
+            userid:userId,
+            courseid:courseid
+        })
         res.json({
-            message: "you are signed up!"
+            message:"course purchases successfully!"
         })
     })
     
-    courseRouter.get("/preview",(req,res)=>{
+    courseRouter.get("/preview",async(req,res)=>{
+
+        const courses = await CourseModel.find({})
         res.json({
-            message: "you are signed up!"
+            courses:courses
+        })
+    })
+
+    courseRouter.get("/purchases",middleware(process.env.JWT_USER_SECRET),async(req,res)=>{
+        const userId = req.userid
+        const purchases = await PurchaseModel.find({
+            userid:userId
+        })
+        const courseData = await CourseModel.find({
+            _id:{ $in: purchases.map(x=>x.courseid)}
+        })
+        console.log(courseData)
+
+        res.json({
+            purchases,
+            courseData
         })
     })
 
