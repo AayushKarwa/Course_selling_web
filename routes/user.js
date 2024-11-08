6bf75f91180cff31eda428859ef9c48bd17dce28
Ayush karwa <ayushkarwa18@gmail.com>
2024-11-09 02:07:25 +0530

    added zod validation

--- routes/user.js
+++ routes/user.js
@@ -4,17 +4,38 @@ const jwt = require("jsonwebtoken")
 const {Router} = require("express");
 const { UserModel } = require("../db");
 const {auth }= require("../auth/auth");
+const bcrypt = require("bcrypt")
+const {z} = require("zod")
 const userRouter = Router()
     
-    
+const signupValidate = z.object({
+    email: z.string().email().min(5).max(25),
+    password: z.string().min(5).max(16),
+    firstname: z.string().min(3).max(16),
+    lastname: z.string().min(3).max(16)
+})
+
+const signinValidate = z.object({
+    email: z.string().email().min(5).max(25),
+    password: z.string().min(5).max(16)
+})
 
     userRouter.post("/signup",async(req,res)=>{
 
-        const {email,password,firstname,lastname } = req.body;
+       const parsedDataWithSuccess = signinValidate.safeParse(req.body)
+       console.log(JSON.stringify(parsedDataWithSuccess))
+       if(!parsedDataWithSuccess.success){
+        res.status(403).json({
+            message: parsedDataWithSuccess.error
+        })
+       }
+        const {email,password,firstname,lastname } = parsedDataWithSuccess.data;
+
+        const hashedPassword = await bcrypt.hash(password,10)
 
         await UserModel.create({
             email:email,
-            password:password,
+            password:hashedPassword,
             firstname:firstname,
             lastname:lastname
         })
@@ -25,11 +46,26 @@ const userRouter = Router()
     
     userRouter.post("/signin",async(req,res)=>{
 
-        const {email,password} = req.body
+        const parsedDataWithSuccess = signinValidate.safeParse(req.body)
+        console.log(parsedDataWithSuccess.data)
+        if(!parsedDataWithSuccess.success){
+            res.status(403).json({
+                message: parsedDataWithSuccess.error
+            })
+        }
+       
+        const {email,password} = parsedDataWithSuccess.data
         const user = await UserModel.findOne({
-            email:email,
-            password:password
+            email:email
+            
         })
+        console.log("user is: "+user)
+        const passwordMatch = await bcrypt.compare(password,user.password)
+        if(!passwordMatch){
+            res.status(403).json({
+                message:"password is incorrect"
+            })
+        }
         console.log("user is: "+ user+ " user id is: "+user._id.toString())
         if(user){ 
 
